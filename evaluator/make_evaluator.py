79d179f5b9c63336599dd12d9887998a8ba8946c
joheeyeon <57741197+joheeyeon@users.noreply.github.com>
2026-01-30 14:31:58 +0900

    Add files via upload

---
+++ evaluator/make_evaluator.py
@@ -0,0 +1,28 @@
+from importlib import import_module
+from dataset.info import DatasetInfo
+
+
+def _evaluator_factory(name, result_dir, anno_file, eval_format, cfg):
+    file = import_module('evaluator.{}.{}'.format(name, 'boundary' if eval_format == 'bound' else 'snake'))
+    if eval_format in ('segm', 'bound'):
+        evaluator = file.Evaluator(result_dir, anno_file, cfg.test)
+    else:
+        evaluator = file.DetectionEvaluator(result_dir, anno_file)
+    return evaluator
+
+
+def make_evaluator(cfg, format=None):
+    name = cfg.test.dataset.split('_')[0]
+    anno_file = DatasetInfo.dataset_info[cfg.test.dataset]['anno_dir']
+    eval_format = cfg.test.segm_or_bbox if format is None else format
+    return _evaluator_factory(name, cfg.commen.result_dir, anno_file, eval_format, cfg)
+
+def _raster_evaluator_factory(name, result_dir, cfg):
+    file = import_module('evaluator.{}.{}'.format(name, 'rasterize'))
+    evaluator = file.Evaluator(result_dir, cfg)
+    return evaluator
+
+
+def make_raster_evaluator(cfg, format=None):
+    name = cfg.test.dataset.split('_')[0]
+    return _raster_evaluator_factory(name, cfg.commen.result_dir, cfg)
